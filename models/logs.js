74e20e418d1c630177cafb05846cf28592192463
James Jensen <jimbob007j@gmail.com>
2015-02-24 16:22:06 -0700

    Modify log model to match bunyan-mongodb-stream

---
+++ models/logs.js
@@ -0,0 +1,30 @@
+/**
+ * The schema of the log entry
+ * @type {Mongoose.Schema}
+ */
+var LogEntrySchema = new mongoose.Schema({
+    msg: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Date,
+        required: true
+    },
+    res : {
+        type: Object
+    },
+    req : {
+        type: Object
+    }
+});
+
+module.exports = mongoose.model('Log', LogEntrySchema);
