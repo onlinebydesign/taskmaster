b9901908a5c02c306f97db3f66843c3ec95ab6f8
James Jensen <jimbob007j@gmail.com>
2015-02-27 12:46:08 -0700

    Get all streams working

--- app.js
+++ app.js
@@ -7,16 +7,13 @@
 // TODO: Move the master and runner in a sub directory
 var config = require('./config.json');
 
-config.role = process.argv[2] || config.role;
+config.role = process.argv[2] || config.role || 'runner';
 
 /**
  * start app based on config file
  */
 if (config.role === 'master') {
-    require('./lib/master')(config.masterPort);
+    require('./lib/master')(config);
 } else if (config.role === 'runner') {
-    require('./lib/runner')(config.masterHost, config.masterPort, {
-        timeout: config.runnerTimeout,
-        maxWorkers: config.runnerMaxWorkers
-    });
+    require('./lib/runner')(config);
 }
