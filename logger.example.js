86f53ecdee4b66faef9e647002172ede64e28018
James Jensen <jimbob007j@gmail.com>
2015-06-16 10:26:07 -0600

    update the logger.example

--- logger.example.js
+++ logger.example.js
@@ -4,46 +4,45 @@ var MongoLogStream = require('bunyan-mongodb-stream');
 var EmailStream = require('bunyan-emailstream').EmailStream;
 var BunyanSlack = require('bunyan-slack');
 
-module.exports = function () {
-    return bunyan.createLogger({
-        name: 'taskmaster',
-        streams: [{
-            level: 'info',
-            stream: process.stdout
+module.exports = bunyan.createLogger({
+    name: 'taskmaster',
+    streams: [{
+        level: 'info',
+        stream: process.stdout
+    }, {
+        level: 'info',
+        type: 'rotating-file',
+        path: './logs/taskmaster-info.log',
+        period: '1d',
+        count: 90
+    }, {
+        level: 'warn',
+        stream: MongoLogStream({model: require('./models/logs')})
+    }, {
+        type: 'raw',
+        level: 'warn',
+        stream: new BunyanSlack({
+            webhook_url: '',
+            channel: '',
+            username: 'Task Master'
+        })
+    }, {
+        type: 'raw',
+        level: 'error',
+        stream: new EmailStream({
+            from: '',
+            to: '',
+            subject: 'Task Master Error',
+            message: 'Something is wrong'
         }, {
-            level: 'info',
-            type: 'rotating-file',
-            path: './logs/taskmaster-info.log',
-            period: '1d',
-            count: 7
-        }, {
-            level: 'warn',
-            stream: MongoLogStream({model: require('models/logs')})
-        }, {
-            type: 'raw',
-            level: 'warn',
-            stream: new BunyanSlack({
-                webhook_url: '',
-                channel: '',
-                username: ''
-            })
-        }, {
-            type: 'raw',
-            level: 'error',
-            stream: new EmailStream({
-                from: '',
-                to: '',
-                subject: 'test email logging',
-                message: 'test'
-            }, {
-                type: 'SMTP',
-                host: "smtp.gmail.com",
-                port: 587,
-                auth: {
-                    user: '',
-                    pass: ''
-                }
-            })
-        }]
-    });
-};
+            type: 'SMTP',
+            host: "smtp.gmail.com",
+            secureConnection: true,
+            port: 465,
+            auth: {
+                user: '',
+                pass: ''
+            }
+        })
+    }]
+});
